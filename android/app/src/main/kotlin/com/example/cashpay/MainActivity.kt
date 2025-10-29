758dffea167ab6ca0225a369aee166cb16b79240
Lukas <lukas581800@gmail.com>
2025-10-28 23:48:22 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/cashpay/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cashpay
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+
+
+void main() async {
+  runApp(const CashPayApp());
+}
+class CashPayApp extends StatelessWidget {
+  const CashPayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
